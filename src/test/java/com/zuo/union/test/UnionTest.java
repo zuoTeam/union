061a7906127df17f2444bb031a77f4c2d15da152
HongyuYao <1014394460@qq.com>
2017-05-24 14:48:56 +0800

    union工程结构。By Yao on May 24th,2017 at 14:48.

---
+++ src/test/java/com/zuo/union/test/UnionTest.java
@@ -0,0 +1,11 @@
+package com.zuo.union.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:application-beans.xml")
+public class UnionTest {
+
+}
