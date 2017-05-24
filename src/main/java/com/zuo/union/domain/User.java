d66846375b47a79bbcbef4fe6336ee51d4a7a994
Hydra <57forgw2@gmail.com>
2017-05-24 16:40:33 +0800

    将实体类的下划线全部去掉了 May 24th by Hydra

--- src/main/java/com/zuo/union/domain/User.java
+++ src/main/java/com/zuo/union/domain/User.java
@@ -14,78 +14,78 @@ public class User implements Serializable{
 	private static final long serialVersionUID = -1382418732730590680L;
 
 	/*用户id*/
-	private Long user_Id;
+	private Long userId;
 	/*用户账号*/
-	private String user_Name;
+	private String userName;
 	/*用户密码*/
-	private String user_Pass;
+	private String userPass;
 	/*用户昵称*/
-	private String user_Nickname;
+	private String userNickname;
 	/*用户电话*/
-	private String user_Tel;
+	private String userTel;
 	/*用户邮箱*/
-	private String user_Email;
+	private String userEmail;
 	/*用户所在小区或社区*/
-	private String user_Community;
+	private String userCommunity;
 	/*用户注册时间*/
-	private Date user_Date;
+	private Date userDate;
 	/*类型id,关联types类：many-to-one,用于区分用户的身份*/
-	private Long type_Id;
+	private Long typeId;
 	
 	
 	public Long getUser_Id() {
-		return user_Id;
+		return userId;
 	}
-	public void setUser_Id(Long user_Id) {
-		this.user_Id = user_Id;
+	public void setUser_Id(Long userId) {
+		this.userId = userId;
 	}
 	public String getUser_Name() {
-		return user_Name;
+		return userName;
 	}
-	public void setUser_Name(String user_Name) {
-		this.user_Name = user_Name;
+	public void setUser_Name(String userName) {
+		this.userName = userName;
 	}
 	public String getUser_Pass() {
-		return user_Pass;
+		return userPass;
 	}
-	public void setUser_Pass(String user_Pass) {
-		this.user_Pass = user_Pass;
+	public void setUser_Pass(String userPass) {
+		this.userPass = userPass;
 	}
 	public String getUser_Nickname() {
-		return user_Nickname;
+		return userNickname;
 	}
-	public void setUser_Nickname(String user_Nickname) {
-		this.user_Nickname = user_Nickname;
+	public void setUser_Nickname(String userNickname) {
+		this.userNickname = userNickname;
 	}
 	public String getUser_Tel() {
-		return user_Tel;
+		return userTel;
 	}
-	public void setUser_Tel(String user_Tel) {
-		this.user_Tel = user_Tel;
+	public void setUser_Tel(String userTel) {
+		this.userTel = userTel;
 	}
 	public String getUser_Email() {
-		return user_Email;
+		return userEmail;
 	}
-	public void setUser_Email(String user_Email) {
-		this.user_Email = user_Email;
+	public void setUser_Email(String userEmail) {
+		this.userEmail = userEmail;
 	}
 	public String getUser_Community() {
-		return user_Community;
+		return userCommunity;
 	}
-	public void setUser_Community(String user_Community) {
-		this.user_Community = user_Community;
+	public void setUser_Community(String userCommunity) {
+		this.userCommunity = userCommunity;
 	}
 	public Date getUser_Date() {
-		return user_Date;
+		return userDate;
 	}
-	public void setUser_Date(Date user_Date) {
-		this.user_Date = user_Date;
+	public void setUser_Date(Date userDate) {
+		this.userDate = userDate;
 	}
 	public Long getType_Id() {
-		return type_Id;
+		return typeId;
 	}
-	public void setType_Id(Long type_Id) {
-		this.type_Id = type_Id;
+	public void setType_Id(Long typeId) {
+		this.typeId = typeId;
 	}
 	
 	
